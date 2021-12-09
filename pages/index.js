e9b8e0dbb7418432fc244664f4f38c4c1b954bef
Helder Santana <heldr@users.noreply.github.com>
2021-12-09 19:15:31 +0000

    test map

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,7 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { MapComponent } from '@components/MapComponent'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <MapComponent style={{ width: "100%", height: "500px" }} latitude={41.8373} longitude={-87.6861} />
   )
 }
