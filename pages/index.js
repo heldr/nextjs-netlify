b0e5b601d209e56fef60839014cea9de0740b345
Helder Santana <heldr@users.noreply.github.com>
2021-12-10 06:25:37 +0000

    reproduce middleware issue

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import { MapComponent } from '@components/MapComponent'
-
-export default function Home() {
-  return (
-    <MapComponent style={{ width: "100%", height: "500px" }} latitude={41.8373} longitude={-87.6861} />
-  )
-}
